3edcf03d399a6d47eea1e0f8baae402217363bf8
Muhammad Reffy <syahnataa@gmail.com>
2024-12-15 11:04:43 +0700

    setup shadcn

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Hello World</h1>
+        <Button>Click Me</Button>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="px-8 py-3 border-b">
+      <h1>Todo List</h1>
+    </header>
+  );
+};
