0db2e80b68f12b91a466c2605f2f7943892cd2e7
Muhammad Reffy <syahnataa@gmail.com>
2024-12-16 14:57:15 +0700

    Designing our footere

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,20 @@ function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className="min-h-screen px-8 mt-4">
         <h1>Hello World</h1>
         <Button>Click Me</Button>
       </main>
+
+      <footer className="px-8 py-3 border-t">
+        <div className="flex justify-between">
+          <a href="#" className="text-2xl font-bold">
+            FYStore
+          </a>
+
+          <Button>Contact Me</Button>
+        </div>
+      </footer>
     </>
   );
 }
