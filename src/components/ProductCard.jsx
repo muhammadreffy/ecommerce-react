496b64e006eb9e58322912975775d7bf15df192b
Muhammad Reffy <syahnataa@gmail.com>
2024-12-16 22:01:51 +0700

    Array of JSX

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,16 @@
-export const ProductCard = () => {
+export const ProductCard = (props) => {
+  const { imageUrl, name, price, stock } = props;
+
   return (
     <div className="flex flex-col p-4 border rounded-md gap-y-4 md:max-w-96">
       <div className="w-full overflow-hidden aspect-square">
-        <img
-          src="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/3/5/3a8003ae-4e9a-465e-aec6-d9487cffb056.jpg"
-          alt="product"
-          className="object-cover"
-        />
+        <img src={imageUrl} alt={name} className="object-cover" />
       </div>
 
       <div>
-        <h2 className="text-lg">The Psychology of Money</h2>
-        <strong className="text-xl">Rp72.250</strong>
-        <p className="text-sm text-muted-foreground">In stock: 10</p>
+        <h2 className="text-lg">{name}</h2>
+        <strong className="text-xl">Rp{price.toLocaleString("id-ID")}</strong>
+        <p className="text-sm text-muted-foreground">In stock: {stock}</p>
       </div>
     </div>
   );
