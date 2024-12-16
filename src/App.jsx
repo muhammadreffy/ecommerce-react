eca3465eacae76e8c0b19165b0b2173bb6d1c996
Muhammad Reffy <syahnataa@gmail.com>
2024-12-16 15:12:01 +0700

    Add a hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,17 @@ function App() {
   return (
     <>
       <Header />
-      <main className="min-h-screen px-8 mt-4">
-        <h1>Hello World</h1>
-        <Button>Click Me</Button>
+      <main className="px-8 mt-4 ">
+        <section className="flex items-center justify-center min-h-[80vh] h-full flex-col gap-y-2">
+          <h1 className="font-bold text-gray-900 sm:text-6xl">
+            Become a trend-setter with us
+          </h1>
+
+          <p className="text-lg text-center max-w-prose">
+            FYStore provides you with the finest clothings and ensures your
+            confidence throughout your days
+          </p>
+        </section>
       </main>
 
       <footer className="px-8 py-3 border-t">
