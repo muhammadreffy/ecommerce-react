eed093cbb75820f3e24e234548b9a9feb8a93870
Muhammad Reffy <syahnataa@gmail.com>
2024-12-18 20:08:45 +0700

    Handle login validation form

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -4,6 +4,7 @@ import {
   CardHeader,
   CardTitle,
   CardFooter,
+  CardDescription,
 } from "../../components/ui/card";
 
 import { Input } from "../../components/ui/input";
@@ -16,57 +17,105 @@ const LoginPage = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
 
+  const [inputUsernameMessage, setInputUsernameMessage] = useState("");
+  const [inputPasswordMessage, setInputPasswordMessage] = useState("");
+
   const [isChecked, setIsChecked] = useState(false);
+
+  const handleLogin = () => {
+    const usernameIsValid = inputEmail.length >= 8;
+    const passwordIsValid = inputPassword.length >= 8;
+
+    if (usernameIsValid && passwordIsValid) {
+      alert(`Username: ${inputEmail}, Password: ${inputPassword}`);
+    }
+  };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-8">
-      <Card className="w-full max-w-lg">
-        <CardHeader>
-          <CardTitle>Welcome back!</CardTitle>
-        </CardHeader>
+      <form className="w-full max-w-lg" onSubmit={handleLogin}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Welcome back!</CardTitle>
+            <CardDescription>
+              Please enter your details to login.
+            </CardDescription>
+          </CardHeader>
 
-        <CardContent className="flex flex-col gap-2">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              onChange={(e) => setInputEmail(e.target.value)}
-            />
-          </div>
+          <CardContent className="flex flex-col gap-2">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                onChange={(e) => {
+                  if (e.target.value.length < 8) {
+                    setInputUsernameMessage(
+                      "Email must be at least 8 characters long."
+                    );
+                  } else {
+                    setInputUsernameMessage("");
+                  }
 
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              id="password"
-              type={isChecked ? "text" : "password"}
-              onChange={(e) => setInputPassword(e.target.value)}
-            />
-          </div>
-
-          <div className="flex items-center mt-2 gap-x-2">
-            <Checkbox
-              id="show-password"
-              onCheckedChange={(checked) => setIsChecked(checked)}
-            />
-
-            <Label htmlFor="show-password">Show password</Label>
-          </div>
-        </CardContent>
-
-        <CardFooter>
-          <div className="flex flex-col w-full">
-            <Button>Login</Button>
-
-            <div className="flex items-center justify-center mt-2 text-gray-700 gap-x-1.5">
-              Don't have an account?
-              <Button variant="link" className="p-0 text-gray-700">
-                Register
-              </Button>
+                  setInputEmail(e.target.value);
+                }}
+              />
+              <p className="text-sm text-muted-foreground">
+                {inputUsernameMessage}
+              </p>
             </div>
-          </div>
-        </CardFooter>
-      </Card>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type={isChecked ? "text" : "password"}
+                onChange={(e) => {
+                  if (e.target.value.length < 8) {
+                    setInputPasswordMessage(
+                      "Password must be at least 8 characters long."
+                    );
+                  } else {
+                    setInputPasswordMessage("");
+                  }
+
+                  setInputPassword(e.target.value);
+                }}
+              />
+              <p className="text-sm text-muted-foreground">
+                {inputPasswordMessage}
+              </p>
+            </div>
+
+            <div className="flex items-center mt-2 gap-x-2">
+              <Checkbox
+                id="show-password"
+                onCheckedChange={(checked) => setIsChecked(checked)}
+              />
+
+              <Label htmlFor="show-password">Show password</Label>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <div className="flex flex-col w-full">
+              <Button
+                type="submit"
+                disabled={inputEmail.length < 8 || inputPassword.length < 8}
+              >
+                Login
+              </Button>
+
+              <div className="flex items-center justify-center mt-2 text-gray-700 gap-x-1.5">
+                Don't have an account?
+                <Button variant="link" className="p-0 text-gray-700">
+                  Register
+                </Button>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </form>
     </main>
   );
 };
