2fcea284a51e11fe4f83c602d776ea35271f1a04
Muhammad Reffy <syahnataa@gmail.com>
2024-12-18 22:13:54 +0700

    implementation react hook form

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -12,110 +12,115 @@ import { Label } from "../../components/ui/label";
 import { Checkbox } from "../../components/ui/checkbox";
 import { Button } from "../../components/ui/button";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormItem,
+  FormMessage,
+  FormLabel,
+  FormField,
+  FormControl,
+} from "../../components/ui/form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginFormSchema = z.object({
+  email: z.string().min(8, "Email has to be 8 characters or more"),
+  password: z.string().min(8, "Your password needs to be 8 characters or more"),
+});
 
 const LoginPage = () => {
-  const [inputEmail, setInputEmail] = useState("");
-  const [inputPassword, setInputPassword] = useState("");
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
 
-  const [inputUsernameMessage, setInputUsernameMessage] = useState("");
-  const [inputPasswordMessage, setInputPasswordMessage] = useState("");
+    resolver: zodResolver(loginFormSchema),
+    mode: "onChange",
+  });
 
   const [isChecked, setIsChecked] = useState(false);
 
-  const handleLogin = () => {
-    const usernameIsValid = inputEmail.length >= 8;
-    const passwordIsValid = inputPassword.length >= 8;
-
-    if (usernameIsValid && passwordIsValid) {
-      alert(`Username: ${inputEmail}, Password: ${inputPassword}`);
-    }
+  const handleLogin = (values) => {
+    console.log(values);
   };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-8">
-      <form className="w-full max-w-lg" onSubmit={handleLogin}>
-        <Card>
-          <CardHeader>
-            <CardTitle>Welcome back!</CardTitle>
-            <CardDescription>
-              Please enter your details to login.
-            </CardDescription>
-          </CardHeader>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleLogin)}
+          className="w-full max-w-lg"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Welcome Back!</CardTitle>
+              <CardDescription>
+                Please enter your details to login.
+              </CardDescription>
+            </CardHeader>
 
-          <CardContent className="flex flex-col gap-2">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                onChange={(e) => {
-                  if (e.target.value.length < 8) {
-                    setInputUsernameMessage(
-                      "Email must be at least 8 characters long."
-                    );
-                  } else {
-                    setInputUsernameMessage("");
-                  }
-
-                  setInputEmail(e.target.value);
-                }}
-              />
-              <p className="text-sm text-muted-foreground">
-                {inputUsernameMessage}
-              </p>
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type={isChecked ? "text" : "password"}
-                onChange={(e) => {
-                  if (e.target.value.length < 8) {
-                    setInputPasswordMessage(
-                      "Password must be at least 8 characters long."
-                    );
-                  } else {
-                    setInputPasswordMessage("");
-                  }
-
-                  setInputPassword(e.target.value);
-                }}
-              />
-              <p className="text-sm text-muted-foreground">
-                {inputPasswordMessage}
-              </p>
-            </div>
-
-            <div className="flex items-center mt-2 gap-x-2">
-              <Checkbox
-                id="show-password"
-                onCheckedChange={(checked) => setIsChecked(checked)}
+            <CardContent className="flex flex-col gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="email" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
 
-              <Label htmlFor="show-password">Show password</Label>
-            </div>
-          </CardContent>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type={isChecked ? "text" : "password"}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <CardFooter>
-            <div className="flex flex-col w-full">
-              <Button
-                type="submit"
-                disabled={inputEmail.length < 8 || inputPassword.length < 8}
-              >
-                Login
-              </Button>
+              <div className="flex items-center mt-2 gap-x-2">
+                <Checkbox
+                  id="show-password"
+                  onCheckedChange={(checked) => setIsChecked(checked)}
+                />
 
-              <div className="flex items-center justify-center mt-2 text-gray-700 gap-x-1.5">
-                Don't have an account?
-                <Button variant="link" className="p-0 text-gray-700">
-                  Register
-                </Button>
+                <Label htmlFor="show-password">Show password</Label>
               </div>
-            </div>
-          </CardFooter>
-        </Card>
-      </form>
+            </CardContent>
+
+            <CardFooter>
+              <div className="flex flex-col w-full">
+                <Button disabled={!form.formState.isValid} type="submit">
+                  Login
+                </Button>
+
+                <div className="flex items-center justify-center mt-2 text-gray-700 gap-x-1.5">
+                  Don't have an account?
+                  <Button variant="link" className="p-0 text-gray-700">
+                    Register
+                  </Button>
+                </div>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </main>
   );
 };
