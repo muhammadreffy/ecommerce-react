3c410278d0d73700dca0b9586f699088d9b7901d
Muhammad Reffy <syahnataa@gmail.com>
2024-12-23 21:15:08 +0700

    added products in table

--- src/pages/admin/ProductManagementPage.jsx
+++ src/pages/admin/ProductManagementPage.jsx
@@ -1,8 +1,39 @@
 import { AdminLayout } from "@/components/layouts/AdminLayout";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { axiosInstance } from "@/lib/axios";
+import { Ellipsis } from "lucide-react";
+import { useEffect, useState } from "react";
 import { IoAdd } from "react-icons/io5";
 
 const ProductManagementPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await axiosInstance.get("/products", {
+        params: {
+          _limit: 5,
+        },
+      });
+
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <AdminLayout
@@ -15,7 +46,32 @@ const ProductManagementPage = () => {
           </Button>
         }
       >
-        <h2>Product Management Page Content</h2>
+        <Table className="p-4 border">
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Stock</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {products.map((product) => (
+              <TableRow>
+                <TableCell>{product.id}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>Rp{product.price.toLocaleString("id-ID")}</TableCell>
+                <TableCell>{product.stock}</TableCell>
+                <TableCell>
+                  <Button variant="ghost" size="icon">
+                    <Ellipsis className="w-6 h-6" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </AdminLayout>
     </>
   );
