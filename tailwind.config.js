5a67a9bfc6e699cc23e39b01f638b1dbc4b1c063
Muhammad Reffy <syahnataa@gmail.com>
2024-12-30 15:13:51 +0700

    apply inter font to web default fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
