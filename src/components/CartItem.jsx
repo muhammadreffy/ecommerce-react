fb5af49a3475c7891dde1bba695707c51f0c2b2e
Muhammad Reffy <syahnataa@gmail.com>
2025-01-13 14:42:39 +0700

    style(cart page): designed cart page item

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,45 @@
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import { Button } from "./ui/button";
+import { IoCheckmark, IoTrash } from "react-icons/io5";
+
+export const CartItem = (props) => {
+  const { name, price, imageUrl } = props;
+  return (
+    <div className="flex gap-4">
+      <div className="w-full overflow-hidden rounded-md aspect-square max-w-52">
+        <img src={imageUrl} alt={name} className="w-full" />
+      </div>
+
+      <div className="flex flex-col justify-between w-full">
+        <div className="flex flex-col">
+          <p>{name}</p>
+          <p className="font-bold">Rp{price.toLocaleString("id-ID")}</p>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant="ghost" size="icon">
+            <IoIosRemove className="w-4 h-4" />
+          </Button>
+
+          <p className="text-lg font-bold">1</p>
+
+          <Button variant="ghost" size="icon">
+            <IoIosAdd className="w-4 h-4" />
+          </Button>
+        </div>
+
+        <div className="flex justify-between w-full">
+          <div className="flex items-center gap-2">
+            <IoCheckmark className="w-6 h-6 text-green-500" />
+            <span className="text-muted-foreground">Available</span>
+          </div>
+
+          <Button variant="ghost">
+            <IoTrash className="w-6 h-6 text-destructive" />
+            <span className="text-destructive">Remove Item</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,27 @@
+import { CartItem } from "@/components/CartItem";
 import { AuthPage } from "@/components/guard/AuthPage";
+import { Separator } from "@/components/ui/separator";
 
 const CartPage = () => {
   return (
     <AuthPage>
-      <div className="min-h-screen">
-        <h2>Cart Page</h2>
-      </div>
+      <main className="max-w-screen-lg min-h-screen px-4 mx-auto mt-8">
+        <h1 className="text-3xl font-bold">My Cart</h1>
+
+        <div className="mt-10">
+          <Separator />
+
+          <div className="grid grid-cols-12 gap-8 my-8">
+            <div className="flex flex-col col-span-7 gap-6">
+              <CartItem
+                name="The Psychology of Money"
+                price={50000}
+                imageUrl="https://images.tokopedia.net/img/cache/900/VqbcmM/2024/3/5/3a8003ae-4e9a-465e-aec6-d9487cffb056.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
     </AuthPage>
   );
 };
