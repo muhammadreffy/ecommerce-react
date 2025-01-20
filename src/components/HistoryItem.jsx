21d5f90ac7a080b104dd51833f0ce8c80fb2b2ab
Muhammad Reffy <syahnataa@gmail.com>
2025-01-20 14:30:41 +0700

    feat(history orders): add order history feature

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -8,60 +8,70 @@ import {
 } from "@/components/ui/table";
 
 export const HistoryItem = (props) => {
-  const { id, dateTime, name, imageUrl, price, quantity, totalPrice, items } =
-    props;
   return (
-    <div className="p-4 border rounded-md">
-      <div className="flex items-center justify-between">
+    <div className="border rounded-md">
+      <div className="flex items-center p-4 justify-between bg-slate-100">
         <div className="flex flex-col justify-center">
-          <span className="text-sm text-muted-foreground">{dateTime}</span>
-
-          <span className="font-semibold text-muted-foreground">INV-{id}</span>
+          <span className="text-sm text-muted-foreground">
+            {new Date(props.dateTime).toLocaleString("id-ID", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            })}
+          </span>
+          <span className="font-semibold text-muted-foreground">
+            INV-{props.id}-FYSTORE
+          </span>
         </div>
 
         <div className="flex flex-col items-end justify-center">
           <span className="text-2xl font-bold">
-            Rp{price.toLocaleString("id-ID")}
+            Rp{props.totalPrice.toLocaleString("id-ID")}
           </span>
         </div>
       </div>
 
-      <Table className="mt-4">
+      <Table className="mt-2">
         <TableHeader>
           <TableRow>
             <TableHead colSpan={2}>Product Name</TableHead>
             <TableHead>Price</TableHead>
             <TableHead>Quantity</TableHead>
+            <TableHead>Tax</TableHead>
             <TableHead>Total Price</TableHead>
           </TableRow>
         </TableHeader>
 
         <TableBody>
-          {items.map((item) => {
-            <TableRow className="font-semibold text-muted-foreground">
+          {props.items.map((item) => (
+            <TableRow
+              key={item.id}
+              className="font-semibold text-muted-foreground"
+            >
               <TableCell colSpan={2}>
                 <div className="flex items-center gap-6">
                   <div className="aspect-square w-[100px] overflow-hidden rounded-md">
                     <img src={item.product.imageUrl} alt={item.product.name} />
                   </div>
-
                   <p className="font-semibold text-primary">
                     {item.product.name}
                   </p>
                 </div>
               </TableCell>
-
               <TableCell>
                 Rp{item.product.price.toLocaleString("id-ID")}
               </TableCell>
-
               <TableCell>{item.quantity}</TableCell>
-
+              <TableCell>Rp{props.tax.toLocaleString("id-ID")}</TableCell>
               <TableCell>
-                Rp{(item.product.price * item.quantity).toLocaleString("id-ID")}
+                Rp{props.totalPrice.toLocaleString("id-ID")}
               </TableCell>
-            </TableRow>;
-          })}
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,14 +1,39 @@
 import { AuthPage } from "@/components/guard/AuthPage";
 import { HistoryItem } from "@/components/HistoryItem";
+import { axiosInstance } from "@/lib/axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const HistoryPage = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  const userSelector = useSelector((state) => state.user);
+
+  const getTransactions = async () => {
+    try {
+      const historyResponse = await axiosInstance.get("/transactions", {
+        params: {
+          userId: userSelector.id,
+        },
+      });
+      setTransactions(historyResponse.data);
+    } catch (error) {
+      console.error("Error fetching transactions:", error);
+    }
+  };
+
+  useEffect(() => {
+    getTransactions();
+  }, []);
+
   return (
     <AuthPage>
       <main className="max-w-screen-lg min-h-screen px-4 mx-auto mt-24">
         <h1 className="text-3xl font-bold">My Orders</h1>
-
         <div className="flex flex-col mt-8 gap-y-24">
-          <HistoryItem />
+          {transactions.map((transaction) => (
+            <HistoryItem key={transaction.id} {...transaction} />
+          ))}
         </div>
       </main>
     </AuthPage>
